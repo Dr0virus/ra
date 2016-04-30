41ff21cd7032d3a5016d5fd7285c3643958c0cd9
Dr0virus <e.nourifard110@gmail.com>
2016-04-30 22:01:08 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -143,8 +143,8 @@ return {
     "^[#!/]([Ss]tats)$",
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
-    "^[#!/]([Ss]tats) (teleseed)",
-	"^[#!/]([Tt]eleseed)"
+    "^[#!/]([Ss]tats) (radioactive)",
+	"^[#!/]([Rr]adioactive)"
     }, 
   run = run
 }

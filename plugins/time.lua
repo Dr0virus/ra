b4cc26504ab6ab6fa10f47fe9bce420d631071f4
Dr0virus <e.nourifard110@gmail.com>
2016-04-30 22:49:32 +0430

    Create time.lua

---
+++ plugins/time.lua
@@ -0,0 +1,13 @@
+do
+function run(msg, matches)
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local text = '⏰ ساعت '..jdat.FAtime..' \n ⌚️ امروز '..jdat.FAdate..' میباشد \n    —--\n⏰'..jdat.ENtime..'\n  ⌚️ '..jdat.ENdate
+return text
+end
+return {
+  patterns = {"^[/!#]([Tt][iI][Mm][Ee])$"}, 
+run = run 
+} 
+end 
